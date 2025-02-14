2d1c7f915bed0ff787b577122557bd9420e96c50
CJ-Meyer <christianmeyerxxx@gmail.com>
2025-02-14 16:04:52 -0500

    init commit

---
+++ Chap17ProdManager/ProductDB.cs
@@ -0,0 +1,51 @@
+﻿using ConsoleLibrary;
+
+namespace Chap17ProdManager
+{
+    internal class ProductDB
+    {
+        private const string Dir = @"D:\C#\Files";
+        private const string Path = Dir + @"\products.txt";
+        private const string Sep = "|";
+        public static void SaveProducts(List<Product> products)
+        {
+            using StreamWriter sw = new StreamWriter(new FileStream(Path, FileMode.Create, FileAccess.Write));
+            if (!Directory.Exists(Dir))
+            {
+                Directory.CreateDirectory(Dir);
+                MyConsole.PrintLine("Directory created.");
+            }
+            foreach (Product p in products)
+            {
+                sw.Write(p.Code + Sep);
+                sw.Write(p.Description + Sep);
+                sw.Write(p.Price);
+            }
+            //sw.Close(); No longer needed thanks to using statement
+        }
+
+        public static List<Product> GetProducts()
+        {
+            using StreamReader sr = new StreamReader(new FileStream(Path, FileMode.OpenOrCreate, FileAccess.Read));
+            List<Product> products = new List<Product>();
+            while (sr.Peek() != -1)
+            {
+                string row = sr.ReadLine();
+                string[] column = row.Split(Sep);
+                if (column.Length == 3)
+                {
+                    string code = column[0];
+                    string description = column[1];
+                    decimal price = decimal.Parse(column[2]);
+                    Product p = new Product(code, description, price);
+                    products.Add(p);
+                }
+
+            }
+            return products;
+        }
+
+
+
+    }
+}
--- Chap17ProdManager/Program.cs
+++ Chap17ProdManager/Program.cs
@@ -10,6 +10,9 @@ namespace Chap17ProdManager {
             // Products will be stored in a txt  file named products.txt
 
             // Load products from file
+
+            products = ProductDB.GetProducts();
+
             int command = 9;
             while (command != 0)
             {
@@ -23,6 +26,12 @@ namespace Chap17ProdManager {
                     case 2:
                         AddProduct();
                         break;
+                    case 3:
+                        UpdateProduct();
+                        break;
+                    case 4:
+                        DeleteProduct();
+                        break;
                     case 0:
                         break;
                     default:
@@ -34,11 +43,86 @@ namespace Chap17ProdManager {
 
             MyConsole.PrintLine("BYE!");
         }
+        private static void DeleteProduct() {
+            MyConsole.PrintLine("\nDelete a product");
+            MyConsole.PrintLine("========================================");
+            string code = MyConsole.PromptString("Enter product code to be deleted: ");
+            bool found = false;
+            for (int i = 0; i < products.Count; i++)
+            {
+                if (products[i].Code == code)
+                {
+                    products.RemoveAt(i);
+                    found = true; // if found, set to true, go to if statement below
+                    break; // Exit after deleting the product
+                }
+            }
+            if (!found) {
+                MyConsole.PrintLine("Code entered is not found in our database.");
+            } else {
+                ProductDB.SaveProducts(products);
+                MyConsole.PrintLine("Product deleted.");
+            }
+        }
+        private static void UpdateProduct() {
+            MyConsole.PrintLine("\nUpdate a product");
+            MyConsole.PrintLine("========================================");
+            string code = MyConsole.PromptString("Enter product code to be updated: ").Trim();
+
+            bool found = false;
+
+            foreach (Product p in products)
+            {
+                if (p.Code == code) 
+                {
+                    string description = MyConsole.PromptString("Enter NEW product description: ").Trim();
+                    decimal price = MyConsole.PromptDecimal("Enter NEW product price: ");
+
+                    p.Description = description;
+                    p.Price = price;
+
+                    found = true;
+                    break; // Stop loop once updated
+                }
+            }
+
+            if (found)
+            {
+                ProductDB.SaveProducts(products);
+                MyConsole.PrintLine("Product updated.");
+            }
+            else
+            {
+                MyConsole.PrintLine("Code entered is not found in our database.");
+            }
+        }
+        private static void AddProduct() {
+            MyConsole.PrintLine("\nAdd a product");
+            MyConsole.PrintLine("========================================");
+            string code = MyConsole.PromptString("Enter product code: ");
+            string description = MyConsole.PromptString("Enter product description: ");
+            decimal price = MyConsole.PromptDecimal("Enter product price: ");
+            Product p = new Product(code, description, price);
+            products.Add(p);
+            ProductDB.SaveProducts(products);
+            MyConsole.PrintLine("Product added.");
+        }
+        private static void ListProducts() {
+            MyConsole.PrintLine("\nList of products");
+            MyConsole.PrintLine("========================================");
+            foreach (Product p in products)
+            {
+                MyConsole.PrintLine(p.ToString());
+            }
+        }
+
         static void DisplayMenu()
         {
             MyConsole.PrintLine("\nCommand Menu:");
-            MyConsole.PrintLine("1) Add a product");
-            MyConsole.PrintLine("2) List all products");
+            MyConsole.PrintLine("1) List all products");
+            MyConsole.PrintLine("2) Add a product");
+            MyConsole.PrintLine("3) Update a product");
+            MyConsole.PrintLine("4) Delete a product");
             MyConsole.PrintLine("0) Exit");
         }
     }
