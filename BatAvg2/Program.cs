2ecb809c8fb1a13ce35ed868806fce656c90985a
christian-meyer <christianmeyerxxx@gmail.com>
2025-02-25 09:08:56 -0500

    init commit

---
+++ BatAvg2/Program.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleLibrary;
+
+namespace BatAvg2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyConsole.PrintLine("Batting Average Calculator");
+            string choice = "y";
+            // print result for at-bat nth time
+
+            // print batting average - # of hits / # of bats
+
+            // print slugging average - total bases ran (points)
+
+            // prompt continue? (y/n)
+            
+
+
+        }
+    }
+}
